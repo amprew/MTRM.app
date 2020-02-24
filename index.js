bf3a163994b39d4883d82734c9fcaef327fc9ecb
Andrew Collins <me@andrewww.co.uk>
2020-02-24 23:44:08 +0000

    Remove express node server

--- index.js
+++
@@ -1,7 +0,0 @@
-require('core-js');
-require('regenerator-runtime');
-const fs = require('fs');
-const babelrc = JSON.parse(fs.readFileSync('./.babelrc'));
-require('@babel/register')(babelrc);
-
-require('./server.js');
